525ffea7d8735282393da87b9d54306902c0fb5c
Agustín Díaz <agustin.ramiro.diaz@gmail.com>
2022-06-01 11:59:35 -0300

    Containerization

--- go-api/server.go
+++ go-api/server.go
@@ -55,5 +55,5 @@ func main() {
 	router.Handle("/graphql", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, router))
+	log.Fatal(http.ListenAndServe("0.0.0.0:"+port, router))
 }
