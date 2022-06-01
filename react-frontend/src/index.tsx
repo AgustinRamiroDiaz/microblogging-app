e1bcc81c8afe87482c373c9a50f296ca5c0702cc
Agustín Díaz <agustin.ramiro.diaz@gmail.com>
2022-06-01 20:44:31 -0300

    Update api uri

--- react-frontend/src/index.tsx
+++ react-frontend/src/index.tsx
@@ -14,11 +14,11 @@ import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
 import { createClient } from "graphql-ws";
 import { getMainDefinition } from "@apollo/client/utilities";
 
-const httpLink = new HttpLink({ uri: "http://localhost:8080/graphql" });
+const httpLink = new HttpLink({ uri: "http://api:8080/graphql" });
 
 const wsLink = new GraphQLWsLink(
   createClient({
-    url: "ws://localhost:8080/graphql",
+    url: "ws://api:8080/graphql",
   })
 );
 
